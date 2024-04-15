8dacee15d4a83479f69653bb2b212f657df71bd3
Eliseu Batista <eliseu.batista@itsector.pt>
2024-04-15 18:03:15 +0100

    Cards Page Base

---
+++ DotNetBankingAppClient/DotNetBankingAppClient/Pages/Cards/Blocks/CardsTabNavigation.cs
@@ -0,0 +1,32 @@
+﻿using DotNetBankingAppClient.Helpers;
+using DotNetBankingAppClient.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DotNetBankingAppClient.Pages;
+
+public class CardsTabNavigationLogic : ComponentBase
+{
+    [Inject]
+    protected IBrowserStorage browserStorage { get; set; } = default!;
+    [Inject]
+    protected NavigationManager navManager { get; set; } = default!;
+
+    public bool isFetching { get; set; } = false;
+    [Parameter]
+    public CardType selectedCardType { get; set; } = CardType.Debit;
+
+    [Parameter]
+    public required Action<CardType> onTabClicked { get; set; }
+
+    public CardType[] tabs = [CardType.Debit, CardType.Credit, CardType.PrePaid];
+
+    public void OnClickTab(CardType tab)
+    {
+        if (onTabClicked != null)
+        {
+            onTabClicked(tab);
+        }
+    }
+
+
+}
---
+++ DotNetBankingAppClient/DotNetBankingAppClient/Pages/Cards/Cards.cs
@@ -0,0 +1,45 @@
+﻿using DotNetBankingAppClient.Constants;
+using DotNetBankingAppClient.Helpers;
+using DotNetBankingAppClient.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DotNetBankingAppClient.Pages;
+
+public class CardsPageLogic : ComponentBase
+{
+    [Inject]
+    protected IBrowserStorage browserStorage { get; set; } = default!;
+    [Inject]
+    protected NavigationManager navManager { get; set; } = default!;
+
+    public bool isFetching { get; set; } = false;
+    public CardType selectedCardType { get; set; } = CardType.Debit;
+    public List<CardDTO> debitCards { get; set; } = new List<CardDTO>();
+    public List<CardDTO> creditCards { get; set; } = new List<CardDTO>();
+    public List<CardDTO> prePaidCards { get; set; } = new List<CardDTO>();
+
+    public void OnClickBack()
+    {
+        navManager.NavigateTo(uri: AppPages.Home, replace: true);
+    }
+
+    public void OnClickTab(CardType tab)
+    {
+        selectedCardType = tab;
+        this.StateHasChanged();
+    }
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        isFetching = true;
+        this.StateHasChanged();
+        var cards = await browserStorage.GetFromLocalStorage<List<CardDTO>>(StoreKeys.Cards);
+        debitCards = cards.Where((card) => card.CardType == CardType.Debit).ToList();
+        creditCards = cards.Where((card) => card.CardType == CardType.Credit).ToList();
+        creditCards = cards.Where((card) => card.CardType == CardType.PrePaid).ToList();
+
+        isFetching = false;
+        this.StateHasChanged();
+    }
+}
