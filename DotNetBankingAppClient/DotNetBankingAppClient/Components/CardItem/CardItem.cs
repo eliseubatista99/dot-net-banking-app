0d3c037fa0e43ab924d41e0db4cc8fb5484cd056
Eliseu Batista <eliseu.batista@itsector.pt>
2024-04-23 18:02:19 +0100

    Card item

---
+++ DotNetBankingAppClient/DotNetBankingAppClient/Components/CardItem/CardItem.cs
@@ -0,0 +1,35 @@
+﻿using DotNetBankingAppClient.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DotNetBankingAppClient.Components;
+
+public class CardItemLogic : ComponentBase
+{
+    [Parameter]
+    public required CardDTO card { get; set; }
+
+    [Parameter]
+    public Action? onClick { get; set; }
+
+    public bool isHovered { get; set; } = false;
+
+    public void HandleOnItemClicked()
+    {
+        if (onClick != null)
+        {
+            onClick();
+        }
+    }
+
+    public void HandleOnItemHovered()
+    {
+        isHovered = true;
+        this.StateHasChanged();
+    }
+
+    public void HandleOnItemUnhovered()
+    {
+        isHovered = false;
+        this.StateHasChanged();
+    }
+}
